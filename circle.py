8470a97edfe3912add303974295ff3b66684a3e3
yaakovizel <z7685727@gmail.com>
2026-05-27 12:44:22 +0300

    added circle class

---
+++ circle.py
@@ -0,0 +1,12 @@
+from calculator import Shape
+from math import pi
+
+class Circle(Shape):
+    def __init__(self,radius):
+        self.radius = radius
+
+    def get_area(self):
+        return pi * self.radius ** 2
+
+    def get_perimeter(self):
+        return 2 * pi * self.radius    
--- traingle.py
+++ traingle.py
@@ -3,15 +3,16 @@ from rectangle import Rectangle
 
 class Traingle(Rectangle):
     def __init__(self, base,height,side_a,side_b,side_c):
-        super().__init__(height)
-        self.base = base
+        super().__init__(base,height)
         self.side_a = side_a
         self.side_b = side_b
         self.side_c = side_c
 
     def get_area(self):
-        return (self.base * self.height) / 2 
+        return super().get_area() / 2 
 
     def get_perimeter(self):
         return self.side_a + self.side_b + self.side_c
        
+t = Traingle(5,7,4,3,2)       
+print(t.get_perimeter())
